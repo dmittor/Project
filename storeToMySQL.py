5c6099b600fd30487a1916ebab0a6cb57a3de325
dmittor <43282595+dmittor@users.noreply.github.com>
2018-09-17 17:24:58 -0400

    a

--- storeToMySQL.py
+++ storeToMySQL.py
@@ -1,4 +1,4 @@
-import MySQLdb
+import PyMySQL
 
 
 class Database:
@@ -8,39 +8,18 @@ class Database:
     password = 'D!i9m9o)N'
     db = 'spiderDb'
 
-    def __init__(self):
-        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
-        self.cursor = self.connection.cursor()
-
-    def insert(self, query):
-        try:
-            self.cursor.execute(query)
-            self.connection.commit()
-        except:
-            self.connection.rollback()
+  spiderDbConnection = pymysql.connect  (host="spiderdb.cnh5suc8nb8k.us-east-1.rds.amazonaws.com",
+  user="admin",
+  passwd="D!i9m9o)N",
+  database="spiderDb")
+spiderDbCursor = spiderDbConnection.cursor()
 
 
 
-    def query(self, query):
-        cursor = self.connection.cursor( MySQLdb.cursors.DictCursor )
-        cursor.execute(query)
-
-        return cursor.fetchall()
-
-    def __del__(self):
-        self.connection.close()
 
 
-if __name__ == "__main__":
 
-    db = Database()
-
-    #CleanUp Operation
-    del_query = "DELETE FROM spiderDb"
-    db.insert(del_query)
-
-    # Data Insert into the table
-    query = """
+query = """
         INSERT INTO spiderDb
         (`name`, `age`)
         VALUES
@@ -49,16 +28,15 @@ if __name__ == "__main__":
         ('Imran', 21)
         """
 
-    # db.query(query)
-    db.insert(query)
 
-    # Data retrieved from the table
-    select_query = """
+spiderDbCursor.execute(query)
+
+select_query = """
         SELECT * FROM spiderDb
         WHERE age = 21
         """
 
-    people = db.query(select_query)
+spiderDbCursor.execute(select_query)
 
-    for person in people:
+for person in spiderDbCursor:
         print ("Found %s " % person['name'])
