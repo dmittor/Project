afb230de6f419113fed8244a816199f9f6c03675
dmittor <43282595+dmittor@users.noreply.github.com>
2018-09-17 21:10:59 -0400

    a

--- First.py
+++ First.py
@@ -17,9 +17,10 @@ class MySpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        self.logger.info('Hi, this is an item page! %s', response.url)
+        print('Hi, this is an item page! %s', response.url)
         item = scrapy.Item()
         item['id'] = response.xpath('//td[@id="item_id"]/text()').re(r'ID: (\d+)')
         item['name'] = response.xpath('//td[@id="item_name"]/text()').extract()
         item['description'] = response.xpath('//td[@id="item_description"]/text()').extract()
         return item
+
