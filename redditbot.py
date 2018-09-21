073c91050bed77c1c2841fea37b997b0f64156fb
dmittor <43282595+dmittor@users.noreply.github.com>
2018-09-21 12:50:20 -0400

    a

--- redditbot.py
+++ redditbot.py
@@ -6,13 +6,45 @@ reddit = praw.Reddit(client_id='8WsvZGB6mbj7sw',
                      user_agent='testscript by /u/grasshopper_api',
                      username='grasshopper_api')
 
-appended_data = []
+
+id_submission_array = []
+author_submission_array = []
+num_comments_submission_array = []
+permalink_submission_array = []
+score_submission_array = []
+title_submission_array = []
+over18_submission_array = []
+upvote_ratio_submission_array = []
+is_video_submission_array = []
+
 
 subreddit = reddit.subreddit('worldnews')
 
-top_python = subreddit.top(limit=10)
+
+
+top_python = subreddit.top(limit=99)
 for submission in top_python:
     if not submission.stickied:
-        appended_data.append(submission.title)
 
-print(appended_data)
+        id_submission_array.append(submission.id)
+        author_submission_array.append(submission.author)
+        num_comments_submission_array.append(submission.num_comments)
+        permalink_submission_array.append(submission.permalink)
+        score_submission_array.append(submission.score)
+        title_submission_array.append(submission.title)
+        over18_submission_array.append(submission.over18)
+        upvote_ratio_submission_array.append(submission.upvote_ratio)
+        is_video_submission_array.append(submission.id)
+
+
+
+
+print(id_submission_array)
+print(author_submission_array)
+print(num_comments_submission_array)
+print(permalink_submission_array)
+print(score_submission_array)
+print(title_submission_array)
+print(over18_submission_array)
+print(upvote_ratio_submission_array)
+print(is_video_submission_array)
