7d056ca5b02ed21923e835da2a4e988fa3563c89
dmittor <43282595+dmittor@users.noreply.github.com>
2018-09-21 20:24:19 -0400

    a

--- storeToMySQL.py
+++ storeToMySQL.py
@@ -9,7 +9,7 @@ class Database:
     db = 'spiderDb'
 
 
-spiderDbConnection = pymysql.connect(host="spiderdb.cnh5suc8nb8k.us-east-1.rds.amazonaws.com", user="admin",  passwd="D!i9m9o)N",  database="spiderDb")
+spiderDbConnection = pymysql.connect(host, user,  password,  db)
 spiderDbCursor = spiderDbConnection.cursor()
 
 query_create = """CREATE TABLE customers (name VARCHAR(255), age int)"""
