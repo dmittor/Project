e8dcfba4765f3b9b64e75460feed4bcd5bb25195
dmittor <43282595+dmittor@users.noreply.github.com>
2018-09-21 20:35:32 -0400

    a

--- storeToMySQL.py
+++ storeToMySQL.py
@@ -7,13 +7,13 @@ class Database:
     spiderDbConnection = pymysql.connect(host="dbgrasshopper.cnh5suc8nb8k.us-east-1.rds.amazonaws.com", user="admin",  passwd="K!u2Z(z0",  database="dbGrasshopper")
     spiderDbCursor = spiderDbConnection.cursor()
 
-query_create = """CREATE TABLE customers (name VARCHAR(255), age int)"""
+    query_create = """CREATE TABLE customers (name VARCHAR(255), age int)"""
 
 
-spiderDbCursor.execute(query_create)
+    spiderDbCursor.execute(query_create)
 
 
-query = """
+    query = """
         INSERT INTO customers
         (`name`, `age`)
         VALUES
@@ -23,18 +23,18 @@ query = """
         """
 
 
-spiderDbCursor.execute(query)
+    spiderDbCursor.execute(query)
 
-select_query = """
+    select_query = """
         SELECT * FROM customers
         WHERE age = 21
         """
 
-spiderDbCursor.execute(select_query)
+    spiderDbCursor.execute(select_query)
 
-for person in spiderDbCursor:
+    for person in spiderDbCursor:
         print(person)
 
-spiderDbCursor.close()
-spiderDbConnection.close()
+    spiderDbCursor.close()
+    spiderDbConnection.close()
 
