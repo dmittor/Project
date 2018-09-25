77ccc7d0519125049bb0fddfe41b205e084c9668
dmittor <43282595+dmittor@users.noreply.github.com>
2018-09-24 21:02:24 -0400

    a

--- redditbot.py
+++ redditbot.py
@@ -55,15 +55,7 @@ except pymysql.InternalError as error:
     print('Got error {!r}, errno is {}'.format(error, error.args[0]))
 
 
-query_create = """CREATE TABLE redditRworldnews ( submission_id VARCHAR(255),
-submission_author VARCHAR(255),
-submission_num_comments int,
-submission_permalink VARCHAR(255),
-submission_score int,
-submission_title VARCHAR(255),
-submission_over_18 VARCHAR(255),
-submission_upvote_ratio float,
-submission_url VARCHAR(512))"""
+query_create = """CREATE TABLE redditRworldnews ( submission_id VARCHAR(255),submission_author VARCHAR(255), submission_num_comments int, submission_permalink VARCHAR(255),submission_score int, submission_title VARCHAR(255),submission_over_18 VARCHAR(255),submission_upvote_ratio float, submission_url VARCHAR(512))"""
 print(query_create)
 try:
     redditDbCursor.execute(query_create)
@@ -71,18 +63,6 @@ except pymysql.InternalError as error:
     print('Got error {!r}, errno is {}'.format(error, error.args[0]))
 
 
-
-query = """
-        INSERT INTO redditRworldnews
-        (submission_id, submission_author, submission_num_comments,submission_permalink, submission_score, submission_title, submission_over_18, submission_upvote_ratio, submission_url)
-        VALUES
-        """+(','.join(map(str, insert_row)))
-
-
-try:
- redditDbCursor.execute(query)
-except pymysql.InternalError as error:
-    print('Got error {!r}, errno is {}'.format(error, error.args[0]))
 
 select_query = """
         SELECT * FROM redditRworldnews
