ac925086bc50f7d506638ff9f2df16bb5afa52b0
dmittor <43282595+dmittor@users.noreply.github.com>
2018-09-24 20:29:22 -0400

    a

--- redditbot.py
+++ redditbot.py
@@ -1,4 +1,5 @@
 import praw
+import pymysql
 
 reddit = praw.Reddit(client_id='8WsvZGB6mbj7sw',
                      client_secret='h9rf_2wMOecGsvUrCbf7QUfCDq0',
@@ -16,8 +17,8 @@ score_submission_array = []
 title_submission_array = []
 over_18_submission_array = []
 upvote_ratio_submission_array = []
-
-
+L = []
+insert_row = []
 
 subreddit = reddit.subreddit('worldnews')
 
@@ -27,67 +28,59 @@ top_python = subreddit.top(limit=5)
 for submission in top_python:
     if not submission.stickied:
 
-        id_submission_array.append(submission.id)
-        author_submission_array.append(submission.author)
-        num_comments_submission_array.append(submission.num_comments)
-        permalink_submission_array.append(submission.permalink)
-        score_submission_array.append(submission.score)
-        title_submission_array.append(submission.title)
-        over_18_submission_array.append(submission.over_18)
-        upvote_ratio_submission_array.append(submission.upvote_ratio)
+        L.append(submission.id)
+        L.append(submission.author)
+        L.append(submission.num_comments)
+        L.append(submission.permalink)
+        L.append(submission.score)
+        L.append(submission.title)
+        L.append(submission.over_18)
+        L.append(submission.upvote_ratio)
+        L.append(submission.url)
 
-        url_submission_array.append(submission.url)
-
-    str1 = ''.join(str(e) for e in list1)
-
-
+        makeitastring = ','.join(map(str, L))
+        insert_row.append("(%)"%makeitastring)
 
 
-    redditDbConnection = pymysql.connect(host="dbgrasshopper.cnh5suc8nb8k.us-east-1.rds.amazonaws.com", user="admin",  passwd="K!u2Z(z0",  database="dbGrasshopper")
-    redditDbCursor = redditDbConnection.cursor()
+redditDbConnection = pymysql.connect(host="dbgrasshopper.cnh5suc8nb8k.us-east-1.rds.amazonaws.com", user="admin",  passwd="K!u2Z(z0",  database="dbGrasshopper")
+redditDbCursor = redditDbConnection.cursor()
 
-    query_create = """CREATE TABLE redditRworldnews (name VARCHAR(255), age int)"""
+query_create = """CREATE TABLE redditRworldnews ( intsubmission_id VARCHAR(255),
+submission_author VARCHAR(255),
+submission_num_comments int,
+submission_permalink VARCHAR(255),
+submission_score int,
+submission_title VARCHAR(255),
+submission_over_18 VARCHAR(255),
+submission_upvote_ratio float,
+submission_url VARCHAR(255))"""
 
 
-    redditDbCursor.execute(query_create)
+redditDbCursor.execute(query_create)
 
 
-    query = """
-        INSERT INTO customers
+query = """
+        INSERT INTO redditRworldnews
         (`name`, `age`)
         VALUES
-        ('Mike', 21),
-        ('Michael', 21),
-        ('Imran', 21)
+        %
+        """%(','.join(map(str, insert_row)))
+
+
+
+redditDbCursor.execute(query)
+
+select_query = """
+        SELECT * FROM redditRworldnews
         """
 
+redditDbCursor.execute(select_query)
 
-    redditDbCursor.execute(query)
+for row in redditDbCursor:
+        print(row)
 
-    select_query = """
-        SELECT * FROM customers
-        WHERE age = 21
-        """
-
-    redditDbCursor.execute(select_query)
-
-    for person in redditDbCursor:
-        print(person)
-
-    redditDbCursor.close()
-    redditDbConnection.close()
+redditDbCursor.close()
+redditDbConnection.close()
 
 
 
-
-
-
-print(id_submission_array)
-print(author_submission_array)
-print(num_comments_submission_array)
-print(permalink_submission_array)
-print(url_submission_array)
-print(score_submission_array)
-print(title_submission_array)
-print(over_18_submission_array)
-print(upvote_ratio_submission_array)
