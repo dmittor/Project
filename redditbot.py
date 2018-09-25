8fe9e8dbdebc54ddb52a4ab1ba7c49d97c6a1a81
dmittor <43282595+dmittor@users.noreply.github.com>
2018-09-24 20:38:30 -0400

    a

--- redditbot.py
+++ redditbot.py
@@ -40,7 +40,7 @@ for submission in top_python:
 
         makeitastring = ','.join(map(str, L))
 
-        insert_row.append('('+ (makeitastring + ')')
+        insert_row.append('(' + makeitastring + ')')
 
 
 redditDbConnection = pymysql.connect(host="dbgrasshopper.cnh5suc8nb8k.us-east-1.rds.amazonaws.com", user="admin",  passwd="K!u2Z(z0",  database="dbGrasshopper")
